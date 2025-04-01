7df68b90c9c55eca21c535540031009a07ab8bc4
javid shabin <javid.main.act@gmail.com>
2025-04-01 19:31:59 +0530

    Create react app and install the router and tailwind css

---
+++ routers/routes.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../layout/Layout";
+import Home from "../src/pages/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { RouterProvider } from 'react-router-dom'
+import { router } from '../routers/routes'
+
+const App = () => {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
